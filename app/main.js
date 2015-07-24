ce9987efd8b31366c1aa5b09de2461c0cce58925
Filip Stachura <filip@appsilon.pl>
2015-07-24 16:36:15 +0200

    Some further work.

--- app/main.js
+++ app/main.js
@@ -1,41 +1,65 @@
-var app = require('app');  // Module to control application life.
-var BrowserWindow = require('browser-window');  // Module to create native browser window.
-var BrowserWindow = require('browser-window');  // Module to create native browser window.
+var app = require('app'),
+    BrowserWindow = require('browser-window'),
+    Menu = require('menu'),
+    runtime = require('./core/runtime'),
+    appMenu = require('./core/app-menu');
 
-// Report crashes to our server.
 require('crash-reporter').start();
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is GCed.
-var mainWindow = null;
+// Load external modules
+var mods = require('./core/modules');
+mods.load(runtime);
 
-// Quit when all windows are closed.
-app.on('window-all-closed', function() {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform != 'darwin') {
+var mainWindow = null;
+var menu = null;
+
+app.on('window-all-closed', function () {
+  //if (process.platform !== 'darwin') {
     app.quit();
-  }
+  //}
 });
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-app.on('ready', function() {
-  // Create the browser window.
-  mainWindow = new BrowserWindow({width: 1400, height: 800});
+app.on('ready', function () {
 
-  // and load the index.html of the app.
+  runtime.emit(runtime.events.INIT_ROUTES, appMenu);
+
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600
+  });
+
+  // initialize runtime reference to main window
+  runtime.windowId = mainWindow.id;
+
   mainWindow.loadUrl('file://' + __dirname + '/index.html');
+  mainWindow.focus();
 
-  // Open the devtools.
-  mainWindow.openDevTools();
-
-  // Emitted when the window is closed.
-  mainWindow.on('closed', function() {
-    console.log("Closed");
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
+  mainWindow.on('closed', function () {
     mainWindow = null;
   });
+
+  // Dock Menu (Mac)
+  if (process.platform === 'darwin') {
+    var dockMenu = Menu.buildFromTemplate([
+      { label: 'New Window', click: function() { console.log('New Window'); } },
+      { label: 'New Window with Settings', submenu: [
+        { label: 'Basic' },
+        { label: 'Pro'},
+      ]},
+      { label: 'New Command...'},
+    ]);
+    app.dock.setMenu(dockMenu);
+  }
+
+  // Application Menu
+  runtime.emit(runtime.events.INIT_APP_MENU, appMenu);
+
+  var template = appMenu.template;
+  menu = Menu.buildFromTemplate(template);
+
+  if (process.platform === 'darwin') {
+    Menu.setApplicationMenu(menu);
+  } else {
+    mainWindow.setMenu(menu);
+  }
 });
